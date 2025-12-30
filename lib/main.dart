53c0bfd2351d7ca5da0f1622b3c71f253ad44c80
Samuel Philip <72622345+ineffablesam@users.noreply.github.com>
2025-12-29 21:13:56 -0800

    feat: change the primary app theme to orange

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Codi',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
       home: const HomePage(title: 'Codi'),
